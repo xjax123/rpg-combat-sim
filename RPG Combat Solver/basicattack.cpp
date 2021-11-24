911596aea1498d99f0f66276b78a38c85606fdc5
Jackson Maclean <xjax1@icloud.com>
2021-11-24 01:46:18 -0800

    Initial creation

--- RPG Combat Solver/basicattack.cpp
+++ RPG Combat Solver/basicattack.cpp
@@ -1,2 +1,25 @@
 //basicattack.cpp
+#pragma once
 
+#include "basicbody.cpp"
+
+//template class, provides basic utilities for constructing attacks
+class attack {
+protected:
+	int damage; //damage per hit
+	int number; //number of hits
+	
+	void setDamage(int dam) {
+
+	}
+
+	void setNumber(int num) {
+
+	}
+public:
+	//basic constructor/destructor
+	attack() {}
+	~attack() {}
+	
+
+};
